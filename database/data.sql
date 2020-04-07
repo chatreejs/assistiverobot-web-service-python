c99ba6c3a39e59abf0eeb1b6b66633675dffd324
Chanon Treemeth <v_chanont@ctlsd.set>
2020-04-07 20:45:26 +0700

    Edit sql file

--- database/data.sql
+++ database/data.sql
@@ -14,21 +14,23 @@ create table goals
 (
     goal_id       int auto_increment
         primary key,
-    jobs_id       int             not null,
-    position_x    decimal(20, 10) not null,
-    position_y    decimal(20, 10) not null,
-    position_z    decimal(20, 10) null,
-    orientation_x decimal(20, 10) not null,
-    orientation_y decimal(20, 10) not null,
-    orientation_z decimal(20, 10) not null,
-    orientation_w decimal(20, 10) not null
+    job_id        int            not null,
+    position_x    decimal(10, 7) not null,
+    position_y    decimal(10, 7) not null,
+    position_z    decimal(10, 7) not null,
+    orientation_x decimal(10, 7) not null,
+    orientation_y decimal(10, 7) not null,
+    orientation_z decimal(10, 7) not null,
+    orientation_w decimal(10, 7) not null
 );
 
 INSERT INTO `assistive_robot`.jobs (job_id, status, created_date, updated_date)
 VALUES (1, 'pending', '2020-04-05 00:00:00', null),
        (2, 'pending', '2020-04-05 00:00:00', null);
 
-INSERT INTO `assistive_robot`.goals (goal_id, jobs_id, position_x, position_y, position_z, orientation_x, orientation_y,
+INSERT INTO `assistive_robot`.goals (goal_id, job_id, position_x, position_y, position_z, orientation_x, orientation_y,
                                      orientation_z, orientation_w)
-VALUES (1, 1, '0', '0', '0', '0', '0', '0', '0'),
-       (2, 1, '0', '0', '0', '0', '0', '0', '0');
+VALUES (1, 1, '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', '1.2345'),
+       (2, 1, '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', '1.2345'),
+       (3, 2, '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', '1.2345'),
+       (4, 2, '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', '1.2345');
