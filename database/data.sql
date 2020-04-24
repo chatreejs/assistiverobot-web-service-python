1271b0a7357ae67bc5b59c28dd637dca2656bab4
Chanon Treemeth <v_chanont@ctlsd.set>
2020-04-24 22:28:44 +0700

    Edit sql file

--- database/data.sql
+++ database/data.sql
@@ -14,26 +14,15 @@ create table goals
 (
 	goal_id int auto_increment
 		primary key,
-	job_id int not null,
-	position_x decimal(10,7) not null,
-	position_y decimal(10,7) not null,
-	position_z decimal(10,7) not null,
-	orientation_x decimal(10,7) not null,
-	orientation_y decimal(10,7) not null,
-	orientation_z decimal(10,7) not null,
-	orientation_w decimal(10,7) not null,
+	job_id int null,
+	position_x decimal(10,7) null,
+	position_y decimal(10,7) null,
+	position_z decimal(10,7) null,
+	orientation_x decimal(10,7) null,
+	orientation_y decimal(10,7) null,
+	orientation_z decimal(10,7) null,
+	orientation_w decimal(10,7) null,
 	status varchar(45) null,
 	constraint goals_jobs_job_id_fk
 		foreign key (job_id) references jobs (job_id)
 );
-
-INSERT INTO `assistive_robot`.jobs (job_id, status, created_date, updated_date)
-VALUES (1, 'pending', '2020-04-05 00:00:00', null),
-       (2, 'pending', '2020-04-05 00:00:00', null);
-
-INSERT INTO `assistive_robot`.goals (goal_id, job_id, position_x, position_y, position_z, orientation_x, orientation_y,
-                                     orientation_z, orientation_w, status)
-VALUES (1, 1, '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', 'pending'),
-       (2, 1, '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', 'pending'),
-       (3, 2, '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', 'pending'),
-       (4, 2, '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', '1.2345', 'pending');
